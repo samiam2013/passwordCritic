5da9312fe3143d7921c78c2dc1813bc6c5347e36
Samuel <samiam2013@gmail.com>
2021-12-28 14:42:39 +0000

    add error level to TooCommonError

--- types/errors.go
+++ types/errors.go
@@ -1,6 +1,10 @@
 package types
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"strings"
+)
 
 // HomogeneityError creates a switchable type of error for low entropy
 type HomogeneityError struct {
@@ -17,11 +21,22 @@ func (h *HomogeneityError) Error() string {
 // TooCommonError creates a switchable type of error for common passwords
 type TooCommonError struct {
 	Occurrences int
+	Level       string
 }
 
 func (t *TooCommonError) Error() string {
-	return fmt.Sprintf("password appears at least %d times in the 'too commmon' "+
-		"passwords list.", t.Occurrences)
+	switch t.Level {
+	case "info":
+		// continue
+	case "warn":
+		// continue
+	case "error":
+		// continue
+	default:
+		log.Fatal("TooCommonError.Level must be 'info', 'warn', or 'error'")
+	}
+	return fmt.Sprintf("%s: password appears at least %d times in the 'too commmon' "+
+		"passwords list.", strings.ToUpper(t.Level), t.Occurrences)
 }
 
 // tie these types to the error interface

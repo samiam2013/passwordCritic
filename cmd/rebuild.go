6c4a559020d72ae55c69a50b10f6861542742980
Samuel <samiam2013@gmail.com>
2022-01-03 02:08:54 +0000

    first iterated working version

--- cmd/rebuild.go
+++ cmd/rebuild.go
@@ -1,25 +1,35 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"io"
+	"os"
 
 	"github.com/samiam2013/passwordcritic/types"
 )
 
 func RebuildFilters() ([]types.BloomFilter, error) {
 	files := map[int]string{
-		1_000:     "./cache/10-million-password-list-top-1000.txt",
-		10_000:    "./cache/10-million-password-list-top-10000",
-		100_000:   "./cache/10-million-password-list-top-100000.txt",
-		1_000_000: "./cache/10-million-password-list-top-1000000.txt",
+		1_000:     "../cache/10-million-password-list-top-1000.txt",
+		10_000:    "../cache/10-million-password-list-top-10000.txt",
+		100_000:   "../cache/10-million-password-list-top-100000.txt",
+		1_000_000: "../cache/10-million-password-list-top-1000000.txt",
 	}
 	filters := make([]types.BloomFilter, 0)
-	for count, filePath := range files {
+	for count, filepath := range files {
 		bitsNeeded := int(float32(count) * 12.364167) // only works for 3 hash functions
-		newFilter, err := buildFilter(count, bitsNeeded, filePath)
+
+		fh, err := os.Open(filepath)
+		if err != nil {
+			return filters, err
+		}
+		defer fh.Close()
+
+		newFilter, err := buildFilter(bitsNeeded, fh)
 		if err != nil {
 			return nil, fmt.Errorf("error building filter for file '%s': %s",
-				filePath, err.Error())
+				fh.Name(), err.Error())
 		}
 		filters = append(filters, *newFilter)
 	}
@@ -27,15 +37,18 @@ func RebuildFilters() ([]types.BloomFilter, error) {
 	return filters, nil
 }
 
-func buildFilter(limit, bits int, filepath string) (bFilter *types.BloomFilter, err error) {
+func buildFilter(bits int, fh io.Reader) (bFilter *types.BloomFilter, err error) {
 	err = nil
 	bFilter = types.NewBloom(bits)
 
+	scanner := bufio.NewScanner(fh)
+	for scanner.Scan() {
+		bFilter.Add(scanner.Bytes())
+	}
+
+	if err = scanner.Err(); err != nil {
+		return
+	}
+
 	return
 }
-
-// what's it like typing on this keyboard instead? not grea
-// 	i NEED to make money, NOW-ish
-
-// it would be hilariously difficult to _not_ get a job in 30 minutes
-//  if I got a kubernetes certificate7

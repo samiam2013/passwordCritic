e3ead5eb36b6e902ba8159db5d45c516e324d18a
Samuel <samiam2013@gmail.com>
2022-01-07 02:31:15 +0000

    added functionality for persisting the built bloomfilters

---
+++ types/bitsetLIst.go
@@ -0,0 +1,56 @@
+package types
+
+import (
+	"encoding/json"
+	"fmt"
+	"os"
+)
+
+// BitsetList holds lists of the built filters for json storage/loading
+type BitsetList struct {
+	// map of the bitsets indexed by # of elements (pws) in the filter
+	List map[int][]bool `json:"list"`
+}
+
+func (bl *BitsetList) LoadFromRebuild(filters map[int]BloomFilter) error {
+	for elems, bFilter := range filters {
+		err := bl.addFilter(elems, bFilter)
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func (bl *BitsetList) addFilter(elems int, b BloomFilter) error {
+	if _, ok := bl.List[elems]; ok {
+		return fmt.Errorf("key (# passwords) '%d' already set", elems)
+	}
+	bl.List[elems] = b.bitset
+	return nil
+}
+
+func (bl *BitsetList) WriteToFile(pathToFile string) error {
+	fh, err := os.Create(pathToFile)
+	if err != nil {
+		return err
+	}
+	defer fh.Close()
+
+	jsonBytes, err := json.Marshal(bl)
+	if err != nil {
+		return err
+	}
+
+	_, err = fh.Write(jsonBytes)
+	return err
+}
+
+func (bl *BitsetList) LoadFromFile(pathToFile string) (list map[int]BloomFilter, err error) {
+	fileBytes, err := os.ReadFile(pathToFile)
+	if err != nil {
+		return
+	}
+	err = json.Unmarshal(fileBytes, bl)
+	return
+}
---
+++ types/bitsetLIst_test.go
@@ -0,0 +1,92 @@
+package types
+
+import (
+	"log"
+	"testing"
+)
+
+// global filters for testing
+var list map[int]BloomFilter
+
+func Init() {
+	var err error
+	list, err = RebuildFilters()
+	if err != nil {
+		log.Fatalf("failed building test case: %s", err.Error())
+	}
+}
+
+func TestBitsetList_LoadFromRebuild(t *testing.T) {
+	type fields struct {
+		List map[int][]bool
+	}
+	type args struct {
+		filters map[int]BloomFilter
+	}
+
+	tests := []struct {
+		name    string
+		fields  fields
+		args    args
+		wantErr bool
+	}{
+		// TODO: Add non-'happy path' test cases.
+		{
+			name: "happy path",
+			fields: fields{
+				List: map[int][]bool{},
+			},
+			args: args{
+				filters: list,
+			},
+			wantErr: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			bl := &BitsetList{
+				List: tt.fields.List,
+			}
+			if err := bl.LoadFromRebuild(tt.args.filters); (err != nil) != tt.wantErr {
+				t.Errorf("BitsetList.LoadFromRebuild() error = %v, wantErr %v", err, tt.wantErr)
+			}
+		})
+	}
+}
+
+func TestBitsetList_WriteToFile(t *testing.T) {
+	type fields struct {
+		List map[int][]bool
+	}
+	type args struct {
+		pathToFile string
+	}
+	tests := []struct {
+		name    string
+		fields  fields
+		args    args
+		wantErr bool
+	}{
+		// TODO: Add test cases.
+		{
+			name: "happy path",
+			fields: fields{
+				List: map[int][]bool{4: {true, false, false, true}},
+			},
+			args: args{
+				pathToFile: "../cache/test_write.json",
+			},
+			wantErr: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			bl := &BitsetList{
+				List: tt.fields.List,
+			}
+			if err := bl.WriteToFile(tt.args.pathToFile); (err != nil) != tt.wantErr {
+				t.Errorf("BitsetList.WriteToFile() error = %v, wantErr %v", err, tt.wantErr)
+			}
+		})
+	}
+}
